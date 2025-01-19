15e7620eccb5af98ceb318a5bcbf9fbb0f0f633c
Jeevan Joshi <joshijeevan98@gmail.com>
2025-01-19 23:08:34 +0530

    complete camera

--- Camera/Camera/CameraApp.swift
+++ Camera/Camera/CameraApp.swift
@@ -2,9 +2,23 @@ import SwiftUI
 
 @main
 struct CameraApp: App {
+    init() {
+        UINavigationBar.applyCustomAppearance()
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CameraView()
         }
+    }
+}
+
+private extension UINavigationBar {
+    static func applyCustomAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 }
